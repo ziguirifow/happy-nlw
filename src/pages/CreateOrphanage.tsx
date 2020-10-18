f4a349d3ba5f3ba46ecbb3e312c69e33eff27e9a
Thulio <thulio.alves@gmail.com>
2020-10-18 16:46:19 -0300

    Abstraindo Sidebar para um componente

--- src/pages/CreateOrphanage.tsx
+++ src/pages/CreateOrphanage.tsx
@@ -1,8 +1,8 @@
 import L from 'leaflet';
 import React from 'react';
-import { FiArrowLeft, FiPlus } from 'react-icons/fi';
+import { FiPlus } from 'react-icons/fi';
 import { Map, Marker, TileLayer } from 'react-leaflet';
-import { useHistory } from 'react-router-dom';
+import Sidebar from '../components/Sidebar';
 import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/create-orphanage.css';
 
@@ -15,39 +15,21 @@ const happyMapIcon = L.icon({
 });
 
 export default function CreateOrphanage() {
-  const { goBack } = useHistory();
-
   return (
     <div id="page-create-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
-
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+      <Sidebar />
 
       <main>
         <form className="create-orphanage-form">
           <fieldset>
             <legend>Dados</legend>
 
-            <Map
-              center={[-27.2092052, -49.6401092]}
-              style={{ width: '100%', height: 280 }}
-              zoom={15}
-            >
+            <Map center={[-23.5505, -46.6333]} style={{ width: '100%', height: 280 }} zoom={15}>
               <TileLayer
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
 
-              <Marker
-                interactive={false}
-                icon={happyMapIcon}
-                position={[-27.2092052, -49.6401092]}
-              />
+              <Marker interactive={false} icon={happyMapIcon} position={[-23.5505, -46.6333]} />
             </Map>
 
             <div className="input-block">
--- src/pages/Orphanage.tsx
+++ src/pages/Orphanage.tsx
@@ -1,9 +1,9 @@
 import L from 'leaflet';
 import React from 'react';
 import { FaWhatsapp } from 'react-icons/fa';
-import { FiArrowLeft, FiClock, FiInfo } from 'react-icons/fi';
+import { FiClock, FiInfo } from 'react-icons/fi';
 import { Map, Marker, TileLayer } from 'react-leaflet';
-import { useHistory } from 'react-router-dom';
+import Sidebar from '../components/Sidebar';
 import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanage.css';
 
@@ -16,19 +16,9 @@ const happyMapIcon = L.icon({
 });
 
 export default function Orphanage() {
-  const { goBack } = useHistory();
-
   return (
     <div id="page-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
-
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+      <Sidebar />
 
       <main>
         <div className="orphanage-details">
@@ -85,7 +75,7 @@ export default function Orphanage() {
 
             <div className="map-container">
               <Map
-                center={[-27.2092052, -49.6401092]}
+                center={[-23.5505, -46.6333]}
                 zoom={16}
                 style={{ width: '100%', height: 280 }}
                 dragging={false}
@@ -97,11 +87,7 @@ export default function Orphanage() {
                 <TileLayer
                   url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
-                <Marker
-                  interactive={false}
-                  icon={happyMapIcon}
-                  position={[-27.2092052, -49.6401092]}
-                />
+                <Marker interactive={false} icon={happyMapIcon} position={[-23.5505, -46.6333]} />
               </Map>
 
               <footer>
