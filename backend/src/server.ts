d0cf1c9a23df71649e2e91dfa510cacdfa3bf94b
Thulio <thulio.alves@gmail.com>
2020-10-18 06:15:29 -0300

    Instalando o Cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
+import cors from 'cors';
 import express from 'express';
 import 'express-async-errors';
 import path from 'path';
 import './database/connection';
-import routes from './routes';
 import errorHandler from './errors/handler';
+import routes from './routes';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
