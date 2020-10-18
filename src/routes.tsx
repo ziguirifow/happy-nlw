ec25692a4f7671f115bd19a9f1421b06e12c63ec
Thulio <thulio.alves@gmail.com>
2020-10-18 16:22:27 -0300

    Finalizando a página de mapa, copiando páginas faltantes e criando navegação entre elas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import CreateOrphanage from './pages/CreateOrphanage';
 import Landing from './pages/Landing';
+import Orphanage from './pages/Orphanage';
 import OrphanagesMap from './pages/OrphanagesMap';
 
 function Routes() {
@@ -9,6 +11,8 @@ function Routes() {
       <Switch>
         <Route path="/" exact component={Landing} />
         <Route path="/app" component={OrphanagesMap} />
+        <Route path="/orphanages/create" component={CreateOrphanage} />
+        <Route path="/orphanages/:id" component={Orphanage} />
       </Switch>
     </BrowserRouter>
   );
