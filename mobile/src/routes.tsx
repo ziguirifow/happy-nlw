543e89f91826ac1ec44bc8313396c16d50e84d92
Thulio <thulio.alves@gmail.com>
2020-10-19 07:42:39 -0300

    Montando rotas do App

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { NavigationAction, NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const { Navigator, Screen } = createStackNavigator();
+
+import OrphanagesMap from './pages/OrphanagesMap';
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="OrphanagesMap" component={OrphanagesMap} />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
